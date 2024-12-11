be8b3e719d73928cbf2ffa9032145368a4272e24
Duaa Rabie <DuaaRabie11@gmail.com>
2024-12-11 08:42:14 -0800

    task0

--- utils/redis.js
+++ utils/redis.js
@@ -1,8 +1,14 @@
 import redis from 'redis';
+import { promisify } from 'util';
+
 
 class RedisClient {
   constructor() {
-    this.client = redis.createClient()
+    this.client = redis.createClient();
+
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.setAsync = promisify(this.client.set).bind(this.client);
+    this.delAsync = promisify(this.client.del).bind(this.client);
 
     this.client.on('error', (err) => {
       console.error('Redis client error:', err);
@@ -10,17 +16,20 @@ class RedisClient {
   }
 
   isAlive() {
-    try {
-      return this.client.ping().then(() => true).catch(() => false);
-    } catch (error) {
-      console.error('Error checking Redis connection:', error);
-      return false;
-    }
+      return new Promise((resolve, reject) => {
+        this.client.ping((error, res) => {
+          if (error) {
+            console.error('Error checking Redis connection:', error);
+            return reject(false);
+          }
+          resolve(res === 'PONG');
+      });
+    });
   }
 
   async get(key) {
     try {
-      const value = await this.client.get(key);
+      const value = await this.getAsync(key);
       return value;
     } catch (error) {
       console.error('Error fetching value from Redis:', error);
@@ -30,7 +39,7 @@ class RedisClient {
 
   async set(key, value, duration) {
     try {
-      await this.client.set(key, value, 'EX', duration);
+      await this.setAsync(key, value, 'EX', duration);
     } catch (error) {
       console.error('Error setting value in Redis:', error);
     }
@@ -38,7 +47,7 @@ class RedisClient {
 
   async del(key) {
     try {
-      await this.client.del(key);
+      await this.delAsync(key);
     } catch (error) {
       console.error('Error deleting key from Redis:', error);
     }
